0f1f36b926f39d22c4cc14435c602f429d154495
mycoolvan <43277405+mycoolvan@users.noreply.github.com>
2018-09-22 15:15:45 -0400

    Moved Collisions to Security

--- Game3/Assets/Scripts/Enemy Scripts/Security.cs
+++ Game3/Assets/Scripts/Enemy Scripts/Security.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Security : MonoBehaviour {
 
@@ -15,9 +16,17 @@ public class Security : MonoBehaviour {
     {
         transform.position = Vector3.MoveTowards(transform.position, playerPos, Time.deltaTime);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
         playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
         if(Vector3.Distance(transform.position, playerPos)<=chaseDistance)
         { 
--- Game3/Assets/Scripts/Player Script/Movement.cs
+++ Game3/Assets/Scripts/Player Script/Movement.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class Movement : MonoBehaviour
 {
@@ -57,13 +57,7 @@ public class Movement : MonoBehaviour
             collision.gameObject.SetActive(false);
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag=="Security")
-        {
-            SceneManager.LoadScene("GameOver");
-        }
-    }
+
 
     void Start()
     {
